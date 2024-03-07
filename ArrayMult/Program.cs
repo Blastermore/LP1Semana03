15cef758c950b3fc65d1dca89b6e3598fd52be25
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-03-07 21:57:14 +0000

    Recretaed the ArrayMult project

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -6,36 +6,7 @@ namespace ArrayMult
     {
         static void Main(string[] args)
         {
-            float [,] matrix = new float [2,2];
-
-            matrix[0,0] = args[0];
-            matrix[0,1] = args[1];
-            matrix[1,0] = args[2];
-            matrix[1,1] = args[3];
-
-            float [] vector = new float [2];
-
-            vector[0] = args[4];
-            vector[1] = args[5];
-
-            float [] vector_final = new float [2];
-            float soma = 0.0;
-
-            //for linhas em amtrix
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    soma += matrix[i,j] * vector[j];
-                }
-                vector_final[i] = soma;
-                soma = 0.0;
-            }  
-
-            foreach(float f in vector_final)
-            {
-                Console.WriteLine(f);
-            }
+            Console.WriteLine("Hello, World!");
         }
     }
 }
