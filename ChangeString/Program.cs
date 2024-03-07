0a59a22d9346fde12a462cf8d1cf3b36175354fd
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-03-07 20:59:39 +0000

    Initialized an empty string with the objective to be the final string to print atthe end; Used a condition to check if the next index in the cycle corresponded to the selected char inputted by the user and if so to replace it with the X; Added a final print to show the final string

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,26 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insere uma string");
+            string s = Console.ReadLine();
+
+            Console.WriteLine("Insere um caracter");
+            char c = char.Parse(Console.ReadLine());
+
+            string s_final = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == c)
+                {
+                    s_final += "X";
+                }
+                else
+                {
+                    s_final += s[i];
+                }
+            }
+            Console.WriteLine(s_final);
         }
     }
 }
