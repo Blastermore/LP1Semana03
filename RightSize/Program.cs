3902288ec72771e2e038000ba0654c959e3e2da0
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-03-07 20:06:28 +0000

    Added the foreach with a basic print for each arg

---
+++ RightSize/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string s in args)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
