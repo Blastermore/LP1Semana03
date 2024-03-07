9fb5dae29adacfa65f03f0a34c7af735a0a4e5a8
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-03-07 23:32:50 +0000

    Restructured entire code and touched up some conditions

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -12,10 +12,6 @@ namespace HeroPerks
 
             string code = args[0];
 
-            int counter_w = 0;
-            int counter_s = 0;
-            int counter_a = 0;
-            int counter_d = 0;
             int mark = 0;
 
             char w = char.Parse("w");
@@ -27,23 +23,22 @@ namespace HeroPerks
             {
                 if (code[i] == w)
                 {
-                    counter_w += 1;
                     myPerk ^= Perks.WaterBreathing;
                 }
                 else if (code[i] == s)
                 {
-                    counter_s += 1;
                     myPerk ^= Perks.Stealth;
+
                 }
                 else if (code[i] == a)
                 {
-                    counter_a += 1;
                     myPerk ^= Perks.AutoHeal;
+
                 }
                 else if (code[i] == d)
                 {
-                    counter_d += 1;
                     myPerk ^= Perks.DoubleJump;
+
                 }
                 else
                 {
@@ -52,26 +47,31 @@ namespace HeroPerks
                     break;
                 }
             }
-            if (myPerk == 0)
+            if (mark == 1)
             {
-                Console.WriteLine("No perks at all!");
+                return;
             }
-            else if (myPerk != 0 && mark != 1)
+            else if (myPerk == 0)
+            {
+                Console.WriteLine("No perks at all");
+            }
+            if (myPerk != 0)
             {
                 Console.WriteLine(myPerk);
-                if ((myPerk & Perks.Stealth) == Perks.Stealth)
-                {
-                    if ((myPerk & Perks.DoubleJump) == Perks.DoubleJump)
-                    {
-                        Console.WriteLine("Silent jumper!");
-                    }
-                }
-                if ((myPerk & Perks.AutoHeal) != Perks.AutoHeal || myPerk == 0)
-                {
-                    Console.WriteLine("Not gonna make it!");
-                }
             }
-                Console.WriteLine(Perks.AutoHeal);
+            {
+            if ((myPerk & Perks.Stealth) == Perks.Stealth)
+            {
+                if ((myPerk & Perks.DoubleJump) == Perks.DoubleJump)
+                {
+                    Console.WriteLine("Silent jumper!");
+                }
+            } 
+            if ((myPerk & Perks.AutoHeal) != Perks.AutoHeal)
+            {
+                Console.WriteLine("Not gonna make it!");
+            }
+            }
         }
     }
 }
