02a7e376cd84ae41357f0b036722752f9f868a29
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-03-07 21:27:11 +0000

    Added the initialization for the matrix and vector; Added a double for loop for the multiplication; Added a final vector variable to print in the end as well as a temporary soma variable to be able to store each values

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -6,7 +6,31 @@ namespace ArrayMult
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            float [,] matrix = new float [2,2];
+
+            matrix[0,0] = args[0];
+            matrix[0,1] = args[1];
+            matrix[1,0] = args[2];
+            matrix[1,1] = args[3];
+
+            float [] vector = new float [2];
+
+            vector[0] = args[4];
+            vector[1] = args[5];
+
+            float [] vector_final = new float [2];
+            float soma = 0.0;
+
+            //for linhas em amtrix
+            for (i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (j = 0; j < matrix.GetLength(1); j++)
+                {
+                    float soma += matrix[i,j] * vector[j];
+                }
+                vector_final[i] = soma;
+                soma = 0.0;
+            }  
         }
     }
 }
