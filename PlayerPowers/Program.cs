e96fe1bde23c1e13099177b8a949baa5b7967cc7
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-03-08 13:56:20 +0000

    Added the whole logic behind the questioning of each player; Added logic to create an array of arrays to store each players powers and the conditions to add the strings to each position of that sub-array

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using HeroPerks;
 
 namespace PlayerPowers
 {
@@ -6,7 +8,51 @@ namespace PlayerPowers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Powers power_player;
+
+            Console.WriteLine("Insira o número de jogadores");
+            int n = int.Parse(Console.ReadLine());
+
+            string [][] player_powers = new string [n][];
+
+            for ( int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Do you have the power 'Fly'?");
+                bool ask_power = bool.Parse(Console.ReadLine());
+
+                if (ask_power)
+                {
+                    player_powers[i][0] =  Powers.Fly.ToString();
+                }
+                Console.WriteLine("Do you have the power 'FXRayVision'?");
+                ask_power = bool.Parse(Console.ReadLine());
+
+                if (ask_power)
+                {
+                    player_powers[i][1] =  Powers.XRayVision.ToString();
+                }
+                Console.WriteLine("Do you have the power 'SuperStrength'?");
+                ask_power = bool.Parse(Console.ReadLine());
+
+                if (ask_power)
+                {
+                    player_powers[i][2] =  Powers.SuperStrength.ToString();
+                }   
+            }
+            for ( int i = 0; i < n; i++)
+            {
+                for (int j =0; j < 3; j++)
+                {
+                    Console.Write(player_powers[i][j]);
+                if (player_powers[i][0] == "Fly" &&
+                player_powers[i][2] == "SuperStrength")
+                {
+                    Console.Write("\nFlying radiation!");
+                }
+                }
+            }
+
+
         }
     }
 }
